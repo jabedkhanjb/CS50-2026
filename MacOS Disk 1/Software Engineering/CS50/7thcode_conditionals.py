2bcb109c8ace9b1540038a26fb2de235ca6ecfbc
Mahfuz Islam Khan Jabed <jabedkhan@macbookpro.mynetworksettings.com>
2026-01-16 02:48:06 -0500

    Add: 7thcode_conditionals.py - conditionals lesson

---
+++ MacOS Disk 1/Software Engineering/CS50/7thcode_conditionals.py
@@ -0,0 +1,9 @@
+x = int(input("What's x?\n"))
+y = int(input("What's y?\n"))
+
+if x < y:
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+else:
+    print("x is equal to y")
